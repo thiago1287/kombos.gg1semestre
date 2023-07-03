d96d000abc6c6fa1fe38a778c5e9991fe73bd63c
Thiago <thiagosilva1@unipam.edu.br>
2023-07-03 02:58:27 -0300

    Alteracoes Finais

--- form.js
+++
@@ -1,9 +0,0 @@
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado ' + nome.value + 'aqui está o link para os kombos https://drive.google.com/drive/folders/1GRwlpXwaORvPFTtVQJbVaN-m8x_Gm3kK?usp=sharing');
-    }
-
-}
